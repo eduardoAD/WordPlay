64ca413458fe6ed402e52ef79430f3fd0408ff02
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-12 00:58:41 -0500

    task #5 complete

---
+++ WordPlay/ResultsViewController.m
@@ -0,0 +1,38 @@
+//
+//  ResultsViewController.m
+//  WordPlay
+//
+//  Created by Eduardo Alvarado Díaz on 10/12/14.
+//  Copyright (c) 2014 Globant. All rights reserved.
+//
+
+#import "ResultsViewController.h"
+
+@interface ResultsViewController ()
+@property (strong, nonatomic) IBOutlet UITextView *resultsTextView;
+
+@end
+
+@implementation ResultsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
