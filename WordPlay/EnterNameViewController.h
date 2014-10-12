2d235695925ff6c60a359b11a87601c2444005e9
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-12 00:46:52 -0500

    task #3 complete

---
+++ WordPlay/EnterNameViewController.h
@@ -0,0 +1,13 @@
+//
+//  EnterNameViewController.h
+//  WordPlay
+//
+//  Created by Eduardo Alvarado Díaz on 10/12/14.
+//  Copyright (c) 2014 Globant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnterNameViewController : UIViewController
+
+@end
