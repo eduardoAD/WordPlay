d780eb8b346b6b918a8850c6e404201590c0fe15
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-12 00:51:26 -0500

    task #4 complete

---
+++ WordPlay/EnterAdjectiveViewController.h
@@ -0,0 +1,13 @@
+//
+//  EnterAdjectiveViewController.h
+//  WordPlay
+//
+//  Created by Eduardo Alvarado Díaz on 10/12/14.
+//  Copyright (c) 2014 Globant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnterAdjectiveViewController : UIViewController
+
+@end
