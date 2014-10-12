5f18badc3adc10251b43c0853675792faadf9271
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-12 03:41:38 -0500

    task #12 complete

--- WordPlay/ResultsViewController.m
+++ WordPlay/ResultsViewController.m
@@ -17,7 +17,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.resultsTextView.text = [NSString stringWithFormat:@"One day %@ and %@ were walking through the forest when suddenly a %@ animal appeared, who live in a %@ house.", self.name, self.otherName, self.adjective, self.otherAdjective];
+    NSString *textString = [NSString stringWithFormat:@"One day %@ and %@ were walking through the forest when suddenly a %@ animal appeared, who live in a %@ house.", self.name, self.otherName, self.adjective, self.otherAdjective];
+
+    NSMutableAttributedString *textAttributedString = [[NSMutableAttributedString alloc] initWithString:textString];
+    [textAttributedString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"HelveticaNeue-Bold" size:16.0] range:[textAttributedString.string rangeOfString:self.name]];
+    [textAttributedString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"HelveticaNeue-Bold" size:16.0] range:[textAttributedString.string rangeOfString:self.otherName]];
+    [textAttributedString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"HelveticaNeue-Bold" size:16.0] range:[textAttributedString.string rangeOfString:self.adjective]];
+    [textAttributedString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"HelveticaNeue-Bold" size:16.0] range:[textAttributedString.string rangeOfString:self.otherAdjective]];
+
+    self.resultsTextView.attributedText = textAttributedString;
+
 }
 
 - (void)didReceiveMemoryWarning {
