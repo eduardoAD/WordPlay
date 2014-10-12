6318742a4f492c2813d8135386ae47cd184390c6
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-11 21:05:45 -0500

    Initial Commit

---
+++ WordPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordPlay
+//
+//  Created by Eduardo Alvarado Díaz on 10/11/14.
+//  Copyright (c) 2014 Globant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WordPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WordPlay
+//
+//  Created by Eduardo Alvarado Díaz on 10/11/14.
+//  Copyright (c) 2014 Globant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
